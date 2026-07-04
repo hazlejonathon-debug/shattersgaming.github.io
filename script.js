a1bf4200a867c6a26da72de9460efca9817a35d4
hazlejonathon-debug <hazlejonathon@gmail.com>
2026-07-04 11:28:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+console.log("Welcome to Shatter's Gaming!");
 
+const buttons = document.querySelectorAll(".button");
+
+buttons.forEach(button=>{
+    button.addEventListener("mouseenter",()=>{
+        button.style.boxShadow="0 0 25px #39ff14";
+    });
+
+    button.addEventListener("mouseleave",()=>{
+        button.style.boxShadow="none";
+    });
+});
+
+window.addEventListener("scroll",()=>{
+
+    const hero=document.querySelector(".hero");
+
+    hero.style.backgroundPositionY=window.scrollY*0.3+"px";
+
+});
