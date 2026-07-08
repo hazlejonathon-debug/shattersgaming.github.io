514b95627b74f3fb656942a8cc6126cdf61d81f0
hazlejonathon-debug <hazlejonathon@gmail.com>
2026-07-08 02:37:32 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -25,7 +25,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-const db = getFirestore(app);
+export const db = getFirestore(app);
 
 window.db = db;
 window.collection = collection;
