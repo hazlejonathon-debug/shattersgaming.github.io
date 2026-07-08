b35378e65f7113baa73df8e2565644b9a19274c6
hazlejonathon-debug <hazlejonathon@gmail.com>
2026-07-08 00:49:00 -0500

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,33 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+
+import {
+    getFirestore,
+    collection,
+    addDoc,
+    serverTimestamp
+} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyDWtyteQ1nli-d6fOlUqd5Rj9O7wmE8O-U",
+
+    authDomain: "shattersgamingstore.firebaseapp.com",
+
+    projectId: "shattersgamingstore",
+
+    storageBucket: "shattersgamingstore.firebasestorage.app",
+
+    messagingSenderId: "1064932478413",
+
+    appId: "1:1064932478413:web:4620aa3228ae9728b58884"
+
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+window.db = db;
+window.collection = collection;
+window.addDoc = addDoc;
+window.serverTimestamp = serverTimestamp;
